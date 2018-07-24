b6edd74a1fbea94d86bd03f1f0086a26331224de
Breno Nunes <breno.arantes97@gmail.com>
2018-07-23 23:32:02 -0300

    String methods additions

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Run the follows command inside of project folder
 > autoreload-server -w "**/**.{html,css,js}" -t ./ -p 8080 -b
 
 Select the file 'index.html' on page start up
+
+To configure ESLint on project install eslint module
+
+> npm install -g eslint
+
+Than, configure a package.json
+
+> npm init
+
+Ends initializing ESLint and configure its
+
+> eslint --init
---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+var msg = 'Hello Word'
+
+console.log('1 - ' + msg.startsWith('H'))
+console.log('2 - ' + msg.endsWith('d'))
+console.log('3 - ' + msg.includes('e'))
+
+console.log('4 - ' + msg.startsWith('W'))
+console.log('5 - ' + msg.endsWith('o'))
+console.log('6 - ' + msg.includes('v'))
+
+console.log('7 - ' + msg.startsWith('l', 2))
+console.log('8 - ' + msg.endsWith('o', 8))
+console.log('9 - ' + msg.includes('o', 5))
+
+console.log('x'.repeat(3))
+console.log('bre'.repeat(2))
+console.log('no'.repeat(5))
